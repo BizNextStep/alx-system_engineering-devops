dd7fd3e8b5726dd56868fa9d4a35478afbf9284d
BizNextStep <charmaine.n.mbatha@icloud.com>
2024-05-07 13:00:16 -0700

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,17 @@
-
 """
 Module for getting the number of subscribers for a given subreddit using the Reddit API.
 """
 
 import requests
 
-def get_subreddit_subscribers(subreddit):
-    """
-    Gets the number of subscribers for a given subreddit using the Reddit API.
-
-    Args:
-        subreddit (str): The name of the subreddit to get the subscribers count for.
-
-    Returns:
-        int: The number of subscribers for the given subreddit, or 0 if the subreddit does not exist.
-    """
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
     url = f"https://api.reddit.com/r/{subreddit}/about.json"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")

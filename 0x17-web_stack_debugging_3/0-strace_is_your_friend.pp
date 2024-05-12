eb251421b898b143f57c28e80a19f057ff18db8c
BizNextStep <charmaine.n.mbatha@icloud.com>
2024-05-12 14:03:32 -0700

    update

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,32 @@
-# This Puppet manifest corrects the file extension from 'phpp' to 'php' in wp-settings.php
+file { '/etc/apache2/mods-available/php7.0.conf':
+  ensure  => file,
+  content => "
+    <FilesMatch \\.php$>
+            SetHandler application/x-httpd-php
+    </FilesMatch>
+    <FilesMatch \\.phps$>
+            SetHandler application/x-httpd-php-source
+            Require all denied
+    </FilesMatch>
+    # Running PHP scripts in user directories is a potential security risk.
+    # User directories are often used to test .php scripts as the user who owns the directory.
+    # This may result in PHP scripts being executed with elevated privileges.
+    # Uncomment the following lines to prevent this behavior
+    #
+    #<Directory /home/*/public_html>
+    #        php_admin_value engine Off
+    #</Directory>
+  ",
+}
 
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin:/bin',
+file { '/etc/apache2/mods-enabled/php7.0.conf':
+  ensure  => link,
+  target  => '/etc/apache2/mods-available/php7.0.conf',
+  require => File['/etc/apache2/mods-available/php7.0.conf'],
+}
+
+service { 'apache2':
+  ensure  => running,
+  enable  => true,
+  require => File['/etc/apache2/mods-enabled/php7.0.conf'],
 }
